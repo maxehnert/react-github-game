433e35239a9ea6b9b3b9010051ee21113f1b5e4e
Max Ehnert <max.ehnert@gmail.com>
2016-03-04 17:39:58 -0600

    Added axios, pulling github data.

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
-
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -17,7 +17,10 @@ var ConfirmBattleContainer = React.createClass({
   },
   componentDidMount: function() {
     var query = this.props.location.query;
-    // Fetch in from github then update state
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function(players) {
+        console.log('players', players);
+      })
     console.log('componentDidMount');
   },
   componentWillReceiveProps: function() {
---
+++ app/utils/githubHelpers.js
@@ -0,0 +1,21 @@
+var axios = require('axios');
+
+var id = 'CLIENT_ID';
+var sec = 'SECRECT_ID';
+var param = '?client_id=' + id + '&client_secret=' + sec;
+function getUserInfo(username) {
+  return axios.get('https://api.github.com/users/' + username + param)
+}
+var helpers = {
+  getPlayersInfo: function(players) {
+    return axios.all(players.map(function(username) {
+      return getUserInfo(username)
+    })).then(function(info) {
+      return info.map(function(user) {
+        return user.data;
+      })
+    })
+  }
+};
+
+module.exports = helpers;
