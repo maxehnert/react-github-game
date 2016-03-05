75d66e4015529a6d7370f92b450419d364e783ac
Max Ehnert <max.ehnert@gmail.com>
2016-03-04 20:15:06 -0600

    Scoring system is functional now.

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -3,8 +3,37 @@ var axios = require('axios');
 var id = 'CLIENT_ID';
 var sec = 'SECRECT_ID';
 var param = '?client_id=' + id + '&client_secret=' + sec;
+
 function getUserInfo(username) {
   return axios.get('https://api.github.com/users/' + username + param)
+}
+
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100')
+}
+
+function getTotalStars(repos) {
+  return repos.data.reduce(function(prev, current) {
+    return prev + current.stargazers_count
+  }, 0)
+}
+
+function getPlayersData(player) {
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function(totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+function calculateScores(players) {
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
 }
 var helpers = {
   getPlayersInfo: function(players) {
@@ -17,6 +46,16 @@ var helpers = {
     }).catch(function(error) {
       console.warn('Error in getPlayersInfo', error);
     })
+  },
+  battle: function(players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function(error) {
+        console.warn('Error in battle', error);
+      });
   }
 };
 
